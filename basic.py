d1f52dcccd86d832425666f6efb7876ac0fcd51a
Asishsts <asishst02@gmail.com>
2026-07-21 14:27:54 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,11 @@
+
+
+
+
+name=(input("Enter Your Name:"))
+age=int(input("Enter Your Age:"))
+
+
+print("My name is %s and I am %d years old."%(name,age))
+
+print(f"My Name is {name} and I am {age}years old.")
